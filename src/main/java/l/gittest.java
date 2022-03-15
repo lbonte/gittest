8d109b4bbb525140af18c39e549ad90c24beda4f
lbt <2445536192@qq.com>
2022-03-15 15:45:22 +0800

    first commit

---
+++ src/main/java/l/gittest.java
@@ -0,0 +1,16 @@
+package l;
+
+/**
+ * @projectName: TT
+ * @package: com.uoh.tt.bean
+ * @className: ServiceRequestLog
+ * @author: Ge
+ * @description: 日志类
+ * @date: 2021/11/12 16:33
+ * @version: 1.0
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
